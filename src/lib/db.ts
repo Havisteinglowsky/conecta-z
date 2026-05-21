5e0719ca08938502e5f83a0e13eacb23db4fce25
Havisteinglowsky <havisteinglowsky@gmail.com>
2026-05-21 02:11:17 -0300

    conectar prisma ao turso

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
 const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined
 }
 
+const libsql = createClient({
+  url: process.env.DATABASE_URL!,
+  authToken: process.env.DATABASE_AUTH_TOKEN!,
+})
+
+const adapter = new PrismaLibSQL(libsql)
+
 export const db =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ['query'],
-  })
+  new PrismaClient({ adapter, log: ['query'] })
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = db
